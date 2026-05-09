0f53274ed4d449be440519a9c170d23504ed445a
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-09 09:21:35 -0400

    intro to js function

---
+++ function.js
@@ -0,0 +1,27 @@
+const price = 4;
+
+// function declare
+function fanOffKor(){
+    console.log('Bosa theke uthe dara');
+    console.log('Walk towards the switch');
+    console.log('Click the switch to off the Fan');
+}
+// function name(){
+//
+// }
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min goso an goso and fena felo');
+    
+}
+
+// call the function
+fanOffKor();
+brushTeeth();
+
+console.log("------------------");
+console.log("------------------");
+console.log("------------------");
+console.log("tom tom ta to");
+brushTeeth();
