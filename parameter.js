57fde04a25be7e5c15f6af5a5046e51a108452f6
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-05-09 09:56:42 -0400

    function parameter

---
+++ parameter.js
@@ -0,0 +1,48 @@
+function vaatKhaw(){
+    console.log('Hand wash kore asho');
+    console.log('Boso');
+    console.log('Plate e khabar dhabar naw');
+    console.log('gopat gopat kore khaw');
+    
+}
+
+// vaatKhaw()
+
+
+// 3 --> 3*3 = 9
+// 4 --> 4*4 = 16
+// 5 --> 5*5 = 25
+// 11 --> 11*11 = 121
+
+function square(number){
+    console.log('value of the number parameter', number);
+    const borgo = number * number;
+    console.log('Square of the given number is: ', borgo);
+    
+}
+
+square(4);
+console.log('-----------------');
+square(12);
+console.log('-----------------');
+square(4);
+console.log('-----------------');
+square(405);
+
+function add(num1, num2){
+    const sum = num1 + num2;
+    console.log(sum);
+    
+}
+
+add(5, 71);
+add(65, 32);
+
+function addAll(a, b, c, d, e){
+    const total = a + b + c + d + e;
+    console.log(total);
+    
+}
+
+addAll(3, 4, 6, 7)
+addAll(3, 4, 6, 7, 8)
